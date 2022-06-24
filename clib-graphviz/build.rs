26b3bc9474989fecc7d3f9fa76cb6464b1343182
NewGyu <newgyu+git@kinoboku.net>
2022-06-24 06:03:40 +0000

    ADD: wasm32 target

--- clib-graphviz/build.rs
+++ clib-graphviz/build.rs
@@ -1,11 +1,17 @@
+use std::{env, path::PathBuf};
 use cmake::Config;
 
 fn main() {
-    let dst = Config::new("cpp")
-        .define("CMAKE_BUILD_TYPE", "MinSizeRel")
-        .define("CMAKE_TOOLCHAIN_FILE", "../emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
-        .build();
-
+    let mut cm = Config::new("cpp");
+    cm.define("CMAKE_BUILD_TYPE", "MinSizeRel");
+    let target = env::var("TARGET").unwrap();
+    if target.contains("wasm32") {
+        let em_cmake = PathBuf::from(env!("EMSDK"))
+            .join("upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake");
+        cm.define("CMAKE_TOOLCHAIN_FILE", em_cmake);
+        cm.target("wasm32-unknown-emscripten");
+    }
+    let dst = &cm.build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=graphvizlib");
 }
