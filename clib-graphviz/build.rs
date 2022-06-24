948c8ab1824b2df8b0d0b6954a4618755fffc5f2
NewGyu <newgyu+git@kinoboku.net>
2022-06-24 10:15:25 +0000

    ADD libs explicitly

--- clib-graphviz/build.rs
+++ clib-graphviz/build.rs
@@ -7,11 +7,41 @@ fn main() {
     let target = env::var("TARGET").unwrap();
     if target.contains("wasm32") {
         let em_cmake = PathBuf::from(env!("EMSDK"))
-            .join("upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake");
+        .join("upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake");
         cm.define("CMAKE_TOOLCHAIN_FILE", em_cmake);
         cm.target("wasm32-unknown-emscripten");
     }
     let dst = &cm.build();
     println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=graphvizlib");
+    for lib in LIBS {
+        println!("cargo:rustc-link-lib=static={}", lib);
+    }
 }
+
+const LIBS:[&str;25] = [
+    "graphvizlib",
+    "cdt",
+    "circogen",
+    "common",
+    "dotgen",
+    "fdpgen",
+    "ingraphs",
+    "label",
+    "neatogen",
+    "ortho",
+    "osage",
+    "pack",
+    "patchwork",
+    "pathplan",
+    "rbtree",
+    "sparse",
+    "sfdpgen",
+    "twopigen",
+    "vpsc",
+    "xdot",
+    "cgraph",
+    "gvc",
+    "gvplugin_core",
+    "gvplugin_dot_layout",
+    "gvplugin_neato_layout"
+];
