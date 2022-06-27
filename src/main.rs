16981dbbf4763231f1a5dc92bec04197143c8aba
NewGyu <newgyu+git@kinoboku.net>
2022-06-27 07:43:03 +0000

    FIX: errors which is caused by rename

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::ffi::CStr;
 
-use clib_graphviz::GraphvizSys;
+use graphviz_sys::GraphvizSys;
 fn main() {
     let v = unsafe {CStr::from_ptr( GraphvizSys::version())}.to_str().unwrap();
     println!("Hello, Graphviz! {}", v);
