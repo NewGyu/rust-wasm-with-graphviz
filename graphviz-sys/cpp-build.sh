6007183d3c0cde7bc00964f525889dd102e96a0c
NewGyu <NewGyu+git@kinoboku.net>
2022-08-02 06:41:06 +0000

    MOD: build script with wasi-sdk

--- graphviz-sys/cpp-build.sh
+++ graphviz-sys/cpp-build.sh
@@ -3,13 +3,11 @@
 SRC_DIR=$(cd $(dirname $0); pwd)
 BUILD_DIR=$SRC_DIR/build
 CMAKE_DIR=$SRC_DIR/cpp
-EMSDK_DIR=$EMSDK
 
-source $EMSDK_DIR/emsdk_env.sh
 if [ ! -d $BUILD_DIR ]; then
     mkdir $BUILD_DIR
     cd $BUILD_DIR
-    cmake $CMAKE_DIR -DCMAKE_TOOLCHAIN_FILE="$EMSDK_DIR/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" -DCMAKE_BUILD_TYPE=MinSizeRel
+    cmake $CMAKE_DIR -DCMAKE_TOOLCHAIN_FILE="$CMAKE_DIR/wasi-sdk.cmake" -DWASI_SDK_PREFIX=$WASI_SDK -DCMAKE_BUILD_TYPE=MinSizeRel
 else
     cd $BUILD_DIR
 fi
